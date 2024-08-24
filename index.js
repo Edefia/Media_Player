48ddc28075384c033ac6e5eaaf25afc34483cbad
Dieu-Donne Edefia Ladzekpo <dieudonneladzekpo@gmail.com>
2024-08-24 08:04:57 +0000

    second Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function redirectMain() {
+
+    window.location = "main.html";
+  
+}
+
+// function redirectSign() {
+//     window.location = "lineloader.html";
+//     setTimeout(() => {
+//         window.location = "signup.html";  
+//     }, 1000);
+// }
+
+function lineLoader() {
+    window.location = "lineloader.html";
+}
